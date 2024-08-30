bddfa3775478e2bd72c879255a937faea8e905d4
unknown <SSDD@PNAWSA05LC1119.duoc.acad>
2024-08-30 09:48:30 -0400

    componentes y angular component

--- src/app/components/componente1/componente1.component.ts
+++ src/app/components/componente1/componente1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-componente1',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Componente1Component  implements OnInit {
 
+  @Input()titulo:string = "";///tiene que estar al inicio de las variables
+  @Input()datos:any;
+  comuna:string ="";
   constructor() { }
 
   ngOnInit() {}
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { Componente1Component } from 'src/app/components/componente1/componente1.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';////agregar la libreria del componente de angular
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatProgressSpinnerModule ///importarlo
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage, Componente1Component]
 })
 export class LoginPageModule {}
